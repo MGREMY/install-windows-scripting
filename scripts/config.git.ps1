0cce5f52df60f2e3db13ea23cf67f0732f21d9fb
MGREMY <mgremy.pro@gmail.com>
2024-10-17 12:55:30 +0200

    fix git config script

--- scripts/config.git.ps1
+++ scripts/config.git.ps1
@@ -1,77 +1,83 @@
-#	configure git script
+# Configure Git script
 
+$homeDirectory = $env:USERPROFILE
 
-$homeDirectory = $env:USERPROFILE;
-
-
-$isMultiConfig = Read-Host "Does the git installation requires multiple .gitconfig files ? [y/yes n/no]";
+$isMultiConfig = Read-Host "Does the git installation require multiple .gitconfig files? [y/yes n/no]"
 
 function Base-Config {
-	echo "Starting base config process";
+    Write-Host "Starting base config process"
 
-	# create ssh directory
-	New-Item -Path "$($homeDirectory)" -Name ".ssh" -ItemType "directory";
-	
-	ssh-keygen -b 4096 -t rsa -f "$(homeDirectory)\.ssh\id_rsa";
+    # Create SSH directory
+    New-Item -Path "$homeDirectory" -Name ".ssh" -ItemType "directory"
 
-	git config --global core.editor "code --wait";
-        git config --global core.autocrlf true;
-        git config --global push.autoSetupRemote true;
-        git config --global rerere.enabled true;
-        git config --global commit.gpgSign true;
-        git config --global gpg.format ssh;
-        git config --global alias.lg "log --graph --abbrev-commit --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'";
-	git config --global user.signingKey "$($homeDirectory)\.ssh\id_rsa.pub";
+    # Generate SSH key
+    ssh-keygen -b 4096 -t rsa -f "$homeDirectory\.ssh\id_rsa"
+
+    # Global Git configuration
+    git config --global core.editor "code --wait"
+    git config --global core.autocrlf true
+    git config --global push.autoSetupRemote true
+    git config --global rerere.enabled true
+    git config --global commit.gpgSign true
+    git config --global gpg.format ssh
+    git config --global alias.lg "log --graph --abbrev-commit --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'"
+    git config --global user.signingKey "$homeDirectory\.ssh\id_rsa.pub"
 }
 
 function Multi-Install {
-	echo "Starting a multi install process";
+    Write-Host "Starting a multi install process"
 
-	Base-Config;
+    Base-Config
 
-	echo "Multi config is going to be setup in the directory $($homeDirectory)\source\repos";
+    Write-Host "Multi config is going to be setup in the directory $homeDirectory\source\repos"
 
-	New-Item -Path "$($homeDirectory)" -Name "source" -ItemType "directory";
-	New-Item -Path "$($homeDirectory)\source" -Name "repos" -ItemType "directory";
+    New-Item -Path "$homeDirectory" -Name "source" -ItemType "directory"
+    New-Item -Path "$homeDirectory\source" -Name "repos" -ItemType "directory"
 
-	$configName = "NONE"
-	while ($configName -ne "") {
-		$configName = Read-Host "What's the name of the configuration (empty if no other configurations) ; name cannot be 'NONE'";
+    $configName = "NONE"
+    while ($configName -ne "") {
+        $configName = Read-Host "What's the name of the configuration (empty if no other configurations); name cannot be 'NONE'"
 
-		New-Item "$($homeDirectory)\source\repos" -Name "$($configName)" -ItemType "directory";
-		$configFilePath = "$($homeDirectory)\source\repos\$($configName)\.gitconfig-$($configName)";
-		New-Item "$($homeDirectory)\source\repos\$($configName)" -Name ".gitconfig-$($configName)" -ItemType "file";
+        if ($configName -ne "") {
+            New-Item -Path "$homeDirectory\source\repos" -Name "$configName" -ItemType "directory"
+            $configFilePath = "$homeDirectory\source\repos\$configName\.gitconfig-$configName"
+            New-Item -Path "$homeDirectory\source\repos\$configName" -Name ".gitconfig-$configName" -ItemType "file"
 
-		$configUserName = Read-Host "What's the git username ?";
-		$configUserEmail = Read-Host "What's the git email ?";
+            $configUserName = Read-Host "What's the git username?"
+            $configUserEmail = Read-Host "What's the git email?"
 
-		$configFileContent = @"
+            $configFileContent = @"
 [user]
-	name = $($configUserName)
-	email = $($configUserEmail)
-";
+    name = $configUserName
+    email = $configUserEmail
+"@
 
-		echo $configFileContent > $configFilePath;
+            Set-Content -Path $configFilePath -Value $configFileContent
 
+            $includeIfConfig = @"
+[includeif "gitdir:~/source/repos/$configName/"]
+    path = ~/source/repos/$configName/.gitconfig-$configName
+"@
 
-		$includeIfConfig = @'
-[includeif "gitdir:~/source/repos/$($configName)/"]
-        path = ~/source/repos/$($configName)/.gitconfig-$($configName)
-';
-	}
-
-		echo $includeIfConfig > "$(Ã$homeDirectory)\.gitconfig"
+            Add-Content -Path "$homeDirectory\.gitconfig" -Value $includeIfConfig
+        }
+    }
 }
 
-function Signle-Install {
-	echo "Starting a single install process";
+function Single-Install {
+    Write-Host "Starting a single install process"
 
-	Base-Config;
+    Base-Config
+
+    $configUserName = Read-Host "What's the git username?"
+    $configUserEmail = Read-Host "What's the git email?"
+
+    git config --global user.name $configUserName
+    git config --global user.email $configUserEmail
 }
 
-If ($isMultiConfig -eq "y" -Or $isMultiConfig -eq "yes") {
-        Multi-Install;
-}
-Else {
-        Single-Install;
+if ($isMultiConfig -eq "y" -or $isMultiConfig -eq "yes") {
+    Multi-Install
+} else {
+    Single-Install
 }
