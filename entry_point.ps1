a77c40196b0217465da4bb03df4fcced4166c70b
MGREMY <mgremy.pro@gmail.com>
2024-10-17 10:16:32 +0200

    add winget installs

---
+++ entry_point.ps1
@@ -0,0 +1,25 @@
+<#
+	entrypoint of the installation process
+	######################################
+	created by : MGREMY
+	created the : 17/10/2024
+	######################################
+#>
+
+#Requires -RunAsAdministrator
+
+# INSTALLATION OF WINGET
+$WINGET_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$WINGET_URL = (Invoke-WebRequest -Uri $WINGET_URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+
+# download
+Invoke-WebRequest -Uri $WINGET_URL -OutFile "winget_setup.msix" -UseBasicParsing
+
+# install
+Add-AppxPackage -Path "winget_setup.msix"
+
+# delete file
+Remove-Item "winget_setup.msix"
---
+++ scripts/winget_installs.ps1
@@ -0,0 +1,18 @@
+<#
+	install software with winget package manager
+#>
+
+winget install -e --id 7zip.7zip;
+winget install -e --id Docker.DockerDesktop;
+winget install -e --id Git.Git;
+winget install -e --id JetBrains.Toolbox;
+winget install -e --id JetBrains.dotUltimate;
+winget install -e --id Logitech.OptionsPlus;
+winget install -e --id Microsoft.DotNet.SDK.8;
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id OpenJS.NodeJS;
+winget install -e --id Notion.Notion;
+winget install -e --id Postman.Postman;
+winget install -e --id Microsoft.PowerToys;
+winget install -e --id VideoLAN.VLC;
+winget install -e --id Microsoft.WindowsTerminal
